c7814594b1890b4a7c7f660ea2453627cfc73510
Dmitrii Tseiler <tseylerd@gmail.com>
2026-02-14 18:02:24 +0100

    update texts

--- MaskThis/UI/UITexts.swift
+++ MaskThis/UI/UITexts.swift
@@ -40,16 +40,16 @@ nonisolated struct UITexts {
         private init() { }
         
         static let privacyTitle = "**Mask This** is private by design."
-        static let privacyNote = "Masking is performed locally using Apple's Foundation Model. Your data never leaves your Mac and is not collected."
+        static let privacyNote = "Masking is performed locally using Apple Foundation Model. Your data never leaves your Mac and is not collected."
         
         static let runsInBackgroundTitle = "The app runs in the background and is accessible via the Menu Bar."
         static let runsInBackgroundNote = "To close the app, select **Quit Mask This** in the menu."
         
         static let modesTitle = "Choose between Auto and Manual modes. Auto is active by default. Toggle **Mask Clipboard Automatically** to switch."
-        static let modesNote = "In Auto mode, the app masks sensitive data every time you copy. In Manual mode, use **Mask Clipboard** or press the shortcut (`Cmd+Shift+M` by default)."
+        static let modesNote = "In Auto mode, the app masks sensitive data in clipboard every time you copy. In Manual mode, use **Mask Clipboard Content** or press the shortcut (`Cmd+Shift+M` by default) to mask content in clipboard."
                 
         static let notificationsTitle = "Stay informed with notifications whenever clipboard content is processing or masked."
-        static let notificationsNote = "You can disable these notifications by switching off the **Show Progress Notifications** and **Show Result Notifications** toggle."
+        static let notificationsNote = "You can disable these notifications by switching off the **Show Progress Notifications** and **Show Result Notifications** toggles."
                 
         static let launchAtLoginTitle = "Start **Mask This** automatically when you log in."
         static let launchAtLoginNote = "To enable this, simply turn on the **Launch at Login** toggle."
@@ -63,7 +63,7 @@ nonisolated struct UITexts {
         static let quit = "Quit Mask This"
         static let help = "Help"
         static let howToUse = "How to Use"
-        static let mask = "Mask Clipboard"
+        static let mask = "Mask Clipboard Content"
         static let about = "About"
         static let ossUsed = "Open Source Software Used"
         static let contactUs = "Contact Us"
